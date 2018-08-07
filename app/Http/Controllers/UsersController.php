3ac7b2dce7c6ad8f0b67b60bd989f45c10b47812
YatShen <379607256@qq.com>
2018-08-07 08:05:30 +0000

    Add user following

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -96,6 +96,20 @@ class UsersController extends Controller
         return back();
     }
 
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(30);
+        $title = '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(30);
+        $title = '粉丝';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
     public function confirmEmail($token)
     {
         $user = User::where('activation_token', $token)->firstOrFail();
